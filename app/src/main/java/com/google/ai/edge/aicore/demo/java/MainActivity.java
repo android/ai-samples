5273cb1b0397c37c50c8979848009e16f4ffb221
Yi Zhou <deanzhou@google.com>
2024-09-18 19:48:14 +0000

    Add required files and headers to pass Cross check.
    Also support inference cancellation for Kotlin sample.
    
    Change-Id: I757bc0f9a99e0fdb893eafe2c3dae629aa4f5fa3

--- app/src/main/java/com/google/ai/edge/aicore/demo/java/MainActivity.java
+++ app/src/main/java/com/google/ai/edge/aicore/demo/java/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.ai.edge.aicore.demo.java;
 
 import android.content.Context;
@@ -169,7 +185,7 @@ public class MainActivity extends AppCompatActivity implements OnConfigUpdateLis
 
   private void endGeneratingUi() {
     sendButton.setEnabled(true);
-    sendButton.setText(R.string.send);
+    sendButton.setText(R.string.button_send);
     streamingSwitch.setEnabled(true);
     configButton.setEnabled(true);
     contentRecyclerView.smoothScrollToPosition(contentAdapter.getItemCount() - 1);
--- app/src/main/java/com/google/ai/edge/aicore/demo/kotlin/MainActivity.kt
+++ app/src/main/java/com/google/ai/edge/aicore/demo/kotlin/MainActivity.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.ai.edge.aicore.demo.kotlin
 
 import android.os.Bundle
@@ -15,8 +31,9 @@ import com.google.ai.edge.aicore.demo.GenerationConfigDialog
 import com.google.ai.edge.aicore.demo.GenerationConfigUtils
 import com.google.ai.edge.aicore.demo.R
 import com.google.ai.edge.aicore.generationConfig
+import java.util.concurrent.Future
 import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.future.future
 
 /** Demonstrates the AICore SDK usage from Kotlin. */
 class MainActivity : AppCompatActivity(), GenerationConfigDialog.OnConfigUpdateListener {
@@ -28,6 +45,8 @@ class MainActivity : AppCompatActivity(), GenerationConfigDialog.OnConfigUpdateL
   private var contentRecyclerView: RecyclerView? = null
   private var model: GenerativeModel? = null
   private var useStreaming = false
+  private var inGenerating = false
+  private var generateContentFuture: Future<Unit>? = null
 
   private val contentAdapter = ContentAdapter()
 
@@ -38,10 +57,16 @@ class MainActivity : AppCompatActivity(), GenerationConfigDialog.OnConfigUpdateL
     requestEditText = findViewById(R.id.request_edit_text)
     sendButton = findViewById(R.id.send_button)
     sendButton!!.setOnClickListener {
-      val request = requestEditText!!.text.toString()
-      contentAdapter.addContent(ContentAdapter.VIEW_TYPE_REQUEST, request)
-      startGeneratingUi()
-      generateContent(request)
+      if (inGenerating) {
+        generateContentFuture?.cancel(true)
+        endGeneratingUi()
+      } else {
+        val request = requestEditText?.text.toString()
+        contentAdapter.addContent(ContentAdapter.VIEW_TYPE_REQUEST, request)
+        startGeneratingUi()
+        generateContent(request)
+      }
+      inGenerating = !inGenerating
     }
 
     streamingSwitch = findViewById<CompoundButton>(R.id.streaming_switch)
@@ -80,51 +105,50 @@ class MainActivity : AppCompatActivity(), GenerationConfigDialog.OnConfigUpdateL
   }
 
   private fun generateContent(request: String) {
-    lifecycleScope.launch {
-      try {
-        if (useStreaming) {
-          var hasFirstStreamingResult = false
-          var result = ""
-          model!!
-            .generateContentStream(request)
-            .onCompletion { endGeneratingUi() }
-            .collect { response ->
-              run {
-                result += response.text
-                if (hasFirstStreamingResult) {
-                  contentAdapter.updateStreamingResponse(result)
-                } else {
-                  contentAdapter.addContent(ContentAdapter.VIEW_TYPE_RESPONSE, result)
-                  hasFirstStreamingResult = true
+    generateContentFuture =
+      lifecycleScope.future {
+        try {
+          if (useStreaming) {
+            var hasFirstStreamingResult = false
+            var result = ""
+            model!!
+              .generateContentStream(request)
+              .onCompletion { endGeneratingUi() }
+              .collect { response ->
+                run {
+                  result += response.text
+                  if (hasFirstStreamingResult) {
+                    contentAdapter.updateStreamingResponse(result)
+                  } else {
+                    contentAdapter.addContent(ContentAdapter.VIEW_TYPE_RESPONSE, result)
+                    hasFirstStreamingResult = true
+                  }
                 }
               }
-            }
-        } else {
-          val response = model!!.generateContent(request)
-          contentAdapter.addContent(ContentAdapter.VIEW_TYPE_RESPONSE, response.text!!)
+          } else {
+            val response = model!!.generateContent(request)
+            contentAdapter.addContent(ContentAdapter.VIEW_TYPE_RESPONSE, response.text!!)
+            endGeneratingUi()
+          }
+        } catch (e: GenerativeAIException) {
+          contentAdapter.addContent(ContentAdapter.VIEW_TYPE_RESPONSE_ERROR, e.message!!)
           endGeneratingUi()
         }
-      } catch (e: GenerativeAIException) {
-        contentAdapter.addContent(ContentAdapter.VIEW_TYPE_RESPONSE_ERROR, e.message!!)
-        endGeneratingUi()
       }
-    }
   }
 
   private fun startGeneratingUi() {
-    sendButton!!.isEnabled = false
-    sendButton!!.setText(R.string.generating)
-    requestEditText!!.setText(R.string.empty)
-    streamingSwitch!!.isEnabled = false
-    configButton!!.isEnabled = false
+    sendButton?.setText(R.string.button_cancel)
+    requestEditText?.setText(R.string.empty)
+    streamingSwitch?.isEnabled = false
+    configButton?.isEnabled = false
   }
 
   private fun endGeneratingUi() {
-    sendButton!!.isEnabled = true
-    sendButton!!.setText(R.string.send)
-    streamingSwitch!!.isEnabled = true
-    configButton!!.isEnabled = true
-    contentRecyclerView!!.smoothScrollToPosition(contentAdapter.itemCount - 1)
+    sendButton?.setText(R.string.button_send)
+    streamingSwitch?.isEnabled = true
+    configButton?.isEnabled = true
+    contentRecyclerView?.smoothScrollToPosition(contentAdapter.itemCount - 1)
   }
 
   override fun onConfigUpdated() {
