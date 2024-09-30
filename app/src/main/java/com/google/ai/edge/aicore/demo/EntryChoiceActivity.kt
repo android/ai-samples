1340a97919dd89d7a48248c175263768210df30e
Yi Zhou <deanzhou@google.com>
2024-09-30 01:01:23 +0000

    Makes sure model is downloaded before trying it, shows download progress otherwise.
    Change-Id: Iabfb65313c8d5beb30b137c7b12084e2f2fd1422

--- app/src/main/java/com/google/ai/edge/aicore/demo/EntryChoiceActivity.kt
+++ app/src/main/java/com/google/ai/edge/aicore/demo/EntryChoiceActivity.kt
@@ -19,27 +19,107 @@ package com.google.ai.edge.aicore.demo
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.google.ai.edge.aicore.DownloadCallback
+import com.google.ai.edge.aicore.DownloadConfig
+import com.google.ai.edge.aicore.GenerativeAIException
+import com.google.ai.edge.aicore.GenerativeModel
 import com.google.ai.edge.aicore.demo.java.MainActivity
+import com.google.ai.edge.aicore.generationConfig
+import java.lang.String.format
+import java.util.Locale
+import kotlinx.coroutines.launch
 
 class EntryChoiceActivity : AppCompatActivity() {
+
+  private var modelDownloaded = false
+  private var model: GenerativeModel? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_entry_choice)
 
     findViewById<TextView>(R.id.kotlin_entry_point).setOnClickListener {
-      val intent =
-        Intent(
-          this@EntryChoiceActivity,
-          com.google.ai.edge.aicore.demo.kotlin.MainActivity::class.java,
-        )
-      startActivity(intent)
+      if (modelDownloaded) {
+        val intent =
+          Intent(
+            this@EntryChoiceActivity,
+            com.google.ai.edge.aicore.demo.kotlin.MainActivity::class.java,
+          )
+        startActivity(intent)
+      } else {
+        Toast.makeText(this, R.string.model_unavailable, Toast.LENGTH_SHORT).show()
+      }
     }
 
     findViewById<TextView>(R.id.java_entry_point).setOnClickListener {
-      val intent = Intent(this@EntryChoiceActivity, MainActivity::class.java)
-      startActivity(intent)
+      if (modelDownloaded) {
+        val intent = Intent(this@EntryChoiceActivity, MainActivity::class.java)
+        startActivity(intent)
+      } else {
+        Toast.makeText(this, R.string.model_unavailable, Toast.LENGTH_SHORT).show()
+      }
     }
+
+    ensureModelDownloaded()
+  }
+
+  private fun ensureModelDownloaded() {
+    val downloadProgressTextView = findViewById<TextView>(R.id.download_progress_text_view)
+    var totalBytesToDownload = 0L
+    val downloadConfig =
+      DownloadConfig(
+        object : DownloadCallback {
+          override fun onDownloadStarted(bytesToDownload: Long) {
+            totalBytesToDownload = bytesToDownload
+          }
+
+          override fun onDownloadFailed(failureStatus: String, e: GenerativeAIException) {
+            Log.e(TAG, "Failed to download model.", e)
+          }
+
+          override fun onDownloadProgress(totalBytesDownloaded: Long) {
+            if (totalBytesToDownload > 0) {
+              downloadProgressTextView?.visibility = View.VISIBLE
+              downloadProgressTextView?.text =
+                format(
+                  Locale.ENGLISH,
+                  "Downloading model:  %d / %d MB (%.2f%%)",
+                  totalBytesDownloaded / MEGABYTE,
+                  totalBytesToDownload / MEGABYTE,
+                  100.0 * totalBytesDownloaded / totalBytesToDownload,
+                )
+            }
+          }
+
+          override fun onDownloadCompleted() {
+            modelDownloaded = true
+          }
+        }
+      )
+
+    model = GenerativeModel(generationConfig { context = applicationContext }, downloadConfig)
+    lifecycleScope.launch {
+      try {
+        model?.prepareInferenceEngine()
+      } catch (e: GenerativeAIException) {
+        Log.e(TAG, "Failed to check model availability.", e)
+        Toast.makeText(applicationContext, e.message, Toast.LENGTH_SHORT).show()
+      }
+    }
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    model?.close()
+  }
+
+  companion object {
+    private const val TAG = "EntryChoiceActivity"
+    private const val MEGABYTE = 1024 * 1024
   }
 }
