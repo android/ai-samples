4b898c361530f1a31bf2fd60e9d8713e2d14bc6a
Thomas Ezan <tezan.enssat@gmail.com>
2025-07-15 16:43:40 +0200

    ui-component module

---
+++ ai-catalog/ui-component/src/main/java/com/android/ai/uicomponent/EmptyComponent.kt
@@ -0,0 +1,9 @@
+package com.android.ai.uicomponent
+
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun EmptyComponent(modifier: Modifier) {
+    // Todo: Implement empty component (...or not :))
+}
