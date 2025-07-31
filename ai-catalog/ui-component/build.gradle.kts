71405ee923269293e3c5c77b38b649191466f463
Thomas Ezan <tezan.enssat@gmail.com>
2025-07-31 10:49:49 -0700

    WIP Tag implementation

--- ai-catalog/ui-component/build.gradle.kts
+++ ai-catalog/ui-component/build.gradle.kts
@@ -39,6 +39,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
             )
         }
     }
