470a820411a610c60561bbebe23a54dbce519eab
Thomas Ezan <tezan.enssat@gmail.com>
2025-08-05 12:57:40 -0700

    Theme update + tag padding adjustment

--- ai-catalog/ui-component/src/main/java/com/android/ai/theme/Theme.kt
+++ ai-catalog/ui-component/src/main/java/com/android/ai/theme/Theme.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.ai.theme
+package com.android.ai.catalog.ui.theme
 
 import android.app.UiModeManager
 import android.content.Context
