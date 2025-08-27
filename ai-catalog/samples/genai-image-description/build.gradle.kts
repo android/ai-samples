c732b36005b6c973201951639e3467ddeb3795f5
Thomas Ezan <tezan.enssat@gmail.com>
2025-08-27 14:41:11 -0700

    Genai Image Description redesign (#86)

--- ai-catalog/samples/genai-image-description/build.gradle.kts
+++ ai-catalog/samples/genai-image-description/build.gradle.kts
@@ -73,5 +73,9 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.kotlinx.coroutines.guava)
     implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.ui.tooling.preview)
+    debugImplementation(libs.ui.tooling)
     ksp(libs.hilt.compiler)
+    implementation(project(":ui-component"))
 }
