2cc27fb7d4921ff358bb50d27ada74233c3e1198
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2025-09-09 16:24:59 +0100

    Merge redesign branch into this so latest changes are available
    Change-Id: I2732cbedb4005d3a0abf6281ee586d7ca356c2f2

--- ai-catalog/samples/gemini-video-summarization/src/main/java/com/android/ai/samples/geminivideosummary/ui/VideoSummarizationScreen.kt
+++ ai-catalog/samples/gemini-video-summarization/src/main/java/com/android/ai/samples/geminivideosummary/ui/VideoSummarizationScreen.kt
@@ -15,30 +15,21 @@
  */
 package com.android.ai.samples.geminivideosummary.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -60,7 +51,6 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
-import com.android.ai.samples.geminivideosummary.player.VideoPlayer
 import com.android.ai.samples.geminivideosummary.util.VideoItem
 import com.android.ai.samples.geminivideosummary.util.sampleVideoList
 import com.android.ai.samples.geminivideosummary.viewmodel.SummarizationState
@@ -68,14 +58,13 @@ import com.android.ai.samples.geminivideosummary.viewmodel.TtsState
 import com.android.ai.samples.geminivideosummary.viewmodel.VideoSummarizationState
 import com.android.ai.samples.geminivideosummary.viewmodel.VideoSummarizationViewModel
 import com.android.ai.theme.AISampleCatalogTheme
-import com.android.ai.theme.extendedColorScheme
 import com.android.ai.uicomponent.GenerateButton
 import com.android.ai.uicomponent.SampleDetailTopAppBar
-import com.android.ai.uicomponent.SecondaryButton
 import com.android.ai.uicomponent.SelectableItem
-import com.android.ai.uicomponent.SelectionDropdown
+import com.android.ai.uicomponent.VideoPickerData
+import com.android.ai.uicomponent.VideoPickerDropdown
+import com.android.ai.uicomponent.VideoPlayer
 import com.google.com.android.ai.samples.geminivideosummary.R
-import java.util.Locale
 
 /**
  * Composable function for the AI Video Summarization screen.
@@ -168,17 +157,28 @@ private fun VideoSummarizationScreen(
                 .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            SelectionDropdown(
-                selectedItem = uiState.selectedVideo?.let {VideoSelectableItem(stringResource(uiState.selectedVideo.titleResId), uiState.selectedVideo) },
-                isDropdownExpanded = isDropdownExpanded,
-                itemList = videoItemList,
-                onItemSelected = {onVideoSelected(it.itemData)},
-                onDropdownExpanded = onDropdownExpandedChanged,
-            )
+//            SelectionDropdown(
+//                selectedItem = uiState.selectedVideo?.let {VideoSelectableItem(stringResource(uiState.selectedVideo.titleResId), uiState.selectedVideo) },
+//                isDropdownExpanded = isDropdownExpanded,
+//                itemList = videoItemList,
+//                onItemSelected = {onVideoSelected(it.itemData)},
+//                onDropdownExpanded = onDropdownExpandedChanged,
+//            )
 
-            if (exoPlayer != null) {
-                VideoPlayer(exoPlayer = exoPlayer, modifier = Modifier.fillMaxWidth())
-            }
+            VideoPlayer(
+                player = exoPlayer,
+                videoPicker = {
+                    VideoPickerDropdown(
+                        videoItems = sampleVideoList.map { VideoPickerData(stringResource(it.titleResId), it.uri) },
+                        selectedVideo = uiState.selectedVideo?.uri,
+                        isExpanded = isDropdownExpanded,
+                        onDropdownExpandedChanged = onDropdownExpandedChanged,
+                        onVideoSelected = { video -> onVideoSelected(sampleVideoList.first { it.uri == video.uri })},
+                    )
+                },
+                forceShowControls = isDropdownExpanded,
+                modifier = Modifier.fillMaxWidth().aspectRatio(16f / 9f),
+            )
 
             SummarizationSection(
                 uiState = uiState,
