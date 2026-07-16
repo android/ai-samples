c168adf7289af64e1257de8d5dffc0121a8ba57d
Tram Bui <trambui@google.com>
2026-07-16 12:12:18 -0700

    remove snapshot build and update glimmer and projected libs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,7 +33,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://androidx.dev/snapshots/builds/15770982/artifacts/repository") }
     }
 }
 
